83600b5d59c1430ecf392ff899ca5639c00fa103
Tim Lindenau <tim.lindenau@outlook.de>
2022-12-17 09:58:00 +0100

    cleanup launch files and add explanatory README for scripts

--- src/tas2-simulator/launch/bringup_simulation.py
+++ src/tas2-simulator/launch/bringup_simulation.py
@@ -6,7 +6,6 @@
 # ===================================================================================
 import os
 import subprocess
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
